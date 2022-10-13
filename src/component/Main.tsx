1018f92f3378db3f19beca8615d75f87289178b5
kekelberi <alexei.gaponov@gmail.com>
2022-10-12 23:36:21 -0500

    save

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -8,7 +8,7 @@ const Main = () => {
       </div>
       <div class="flex justify-around text-blue-700">
         <A href="/">one</A>
-        <A href="/two">two</A>
+        <A  href="/two">two</A>
       </div>
     </>
   );
