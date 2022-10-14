ac3916736ff3cd23dc0d78f486fa6d8d74d595b2
kekelberi <alexei.gaponov@gmail.com>
2022-10-14 11:06:10 -0500

    save

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -1,5 +1,18 @@
+import s from "../../scss/layout.module.scss";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div class={s.header_main}>
+      <h1>gap_hap</h1>
+      <div>
+        <ul>
+          <li>home</li>
+          <li>project</li>
+          <li>contacts</li>
+          <li>about</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
