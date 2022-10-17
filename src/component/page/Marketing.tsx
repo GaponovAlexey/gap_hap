6b4e62ead99c1a794ab5e29875bab45f11142b57
kekelberi <alexei.gaponov@gmail.com>
2022-10-17 15:28:09 -0500

    save

--- src/component/page/Marketing.tsx
+++ src/component/page/Marketing.tsx
@@ -1,6 +1,13 @@
-import s from '../../scss/marketing.module.scss'
+import s from "../../scss/marketing.module.scss";
+
 const Marketing = () => {
-  return <div class={s.main_marketing}>marketing</div>;
+  return (
+    <div class={s.main_marketing}>
+      <h2>mar</h2>
+      <div class={s.marketing_page}>marketing</div>
+      <div class={s.marketing_page}>marketing</div>
+    </div>
+  );
 };
 
 export default Marketing;
