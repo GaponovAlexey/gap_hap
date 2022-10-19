2f288740aaba22ed36dc9560e30060e0c91b9864
kekelberi <alexei.gaponov@gmail.com>
2022-10-18 20:59:23 -0500

    save

--- src/component/page/landing/LandingUse.tsx
+++ src/component/page/landing/LandingUse.tsx
@@ -1,15 +1,15 @@
 const LandingUse = () => {
   return (
     <section class="text-gray-400 body-font ">
-      <div class=" px-5 py-24 mx-auto">
+      <div class=" px-5 py-14 mx-auto">
         <div class="flex flex-col text-center w-full mb-10">
-          <h2 class="text-xs text-indigo-400 tracking-widest font-medium title-font mb-1">
+          <h2 class="text-xs text-indigo-400 tracking-widest font-medium  mb-1">
             Why you need a website for your small business
           </h2>
-          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font  mb-4 text-white">
             A website makes you look professional
           </h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p class="lg:w-2/3 mx-auto leading-relaxed ">
             84% of today’s consumers think a website makes your business more
             credible than companies who only have social media profiles. Your
             website is also the perfect place to show off any professional
@@ -17,25 +17,36 @@ const LandingUse = () => {
           </p>
         </div>
         <div class="flex flex-wrap">
-          <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-            <h2 class="text-lg sm:text-xl text-white font-medium title-font mb-2">
-              Shooting Stars
+          <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 sm py-6 border-l-2 border-gray-800">
+            {/* 2 */}
+            <h2 class="text-lg sm:text-xl text-white">
+              Your website can attract new customers through Google
             </h2>
             <p class="leading-relaxed text-base mb-4">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
+              You may be happy with the current size of your business, but every
+              company experiences customer turnover. To encourage continued
+              success, you need to attract new customers, and one of the best
+              ways to do it is by making yourself more visible on Google. Social
+              media networks are indexed on Google, and social media posts can
+              be found with the search engine, but a website gives you access to
+              many more tools and strategies for Search Engine Optimization
+              (SEO). And SEO is the key to making your business appear on the
+              front page of Google.
             </p>
           </div>
           <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
+            {/* 3 */}
             <h2 class="text-lg sm:text-xl text-white font-medium title-font mb-2">
-              The Catalyzer
+              You can clearly showcase your products and services
             </h2>
             <p class="leading-relaxed text-base mb-4">
-              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-              hexagon disrupt edison bulbche.
+              You can show potential customers what they’ll get when they work
+              with you by displaying high-quality photography on your website. <br />
+              You can also use your website design, along with the featured images, to give people a sense of what it feels like to enter your physical location. This works particularly well if your brand is closely tied to the “feel” of your location, like the Canadian restaurant
             </p>
           </div>
           <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
+            {/* 4 */}
             <h2 class="text-lg sm:text-xl text-white font-medium title-font mb-2">
               Neptune
             </h2>
