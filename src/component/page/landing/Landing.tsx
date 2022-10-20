d47ab63445bb6e37d5c9e089ba8f551a75026741
kekelberi <alexei.gaponov@gmail.com>
2022-10-20 01:03:47 -0500

    save

--- src/component/page/landing/Landing.tsx
+++ src/component/page/landing/Landing.tsx
@@ -5,9 +5,7 @@ const Lending = () => {
   return (
     <div class={s.landing_main}>
       <LandingUse />
-      <div class={s.landing_top}>
-        <div></div>
-      </div>
+      <div class={s.landing_top}></div>
       <div class={s.landing_top}>da</div>
     </div>
   );
