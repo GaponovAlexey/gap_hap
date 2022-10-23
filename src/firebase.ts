18b7165cec31a8d9fdfc0e5acc51a5ff2580f4c7
kekelberi <alexei.gaponov@gmail.com>
2022-10-23 17:47:52 -0500

    save

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/firestore";
-import { getFirestore } from "firebase/firestore";
+// import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,5 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-export const db = getFirestore(app)
+// export const db = getFirestore(app)
 
