ea508b9a1b5633a1e75a626cc4f4e4c4157466eb
kekelberi <alexei.gaponov@gmail.com>
2022-10-22 20:27:35 -0500

    save

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
+const firebaseConfig = initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTHOR_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
@@ -9,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.FIREBASE_MES_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEAS_ID,
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+});
+
+const analytics = getAnalytics(firebaseConfig);
+
