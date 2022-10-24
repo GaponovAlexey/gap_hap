fc20175332aa4afda18c89f55a71bad9b2a7a22a
kekelberi <alexei.gaponov@gmail.com>
2022-10-23 20:02:10 -0500

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Component } from "solid-js";
 import Layout from "./component/layout";
-import Main from "./component/Main";
 import Pages from "./Pages";
 
 const App: Component = () => {
-  // console.log("env", import.meta.env.VITE_FIREBASE_API_KEY);
   return (
     <>
       <Layout>
