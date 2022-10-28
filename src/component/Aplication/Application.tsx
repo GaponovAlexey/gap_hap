07c9ff23a26a084c5b5dc00d4976827ab1d79876
kekelberi <alexei.gaponov@gmail.com>
2022-10-28 14:37:44 -0500

    save

--- src/component/Aplication/Application.tsx
+++ src/component/Aplication/Application.tsx
@@ -1,40 +1,81 @@
-import { collection, getDocs, getDocsFromCache } from "firebase/firestore";
-import { createSignal, For, Suspense } from "solid-js";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getDocsFromCache,
+  updateDoc,
+} from "firebase/firestore";
+import { Input } from "postcss";
+import { createEffect, createSignal, For, Suspense } from "solid-js";
 import { createStore } from "solid-js/store";
 import { db } from "../../../firebase";
 
 const Application = () => {
   const [state, setState] = createSignal<any>();
+  const usersColRef = collection(db, "users");
+  const [name, setName] = createSignal<any>("");
 
-  const getUsers = async () => {
-    const usersColRef = collection(db, "users");
-    const data = await getDocs(usersColRef);
-    setState(data.docs.map((e: any) => ({ ...e.data(), id: e.id })));
-    console.log(
-      "da",
-      data.docs.map((e: any) => ({ ...e.data() }))
-    );
+  //GET
+  createEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersColRef);
+      setState(data.docs.map((e: any) => ({ ...e.data(), id: e.id })));
+    };
+    getUsers();
+  }, [setState]);
+
+  //POST
+  const add = async (e: any) => {
+    e.preventDefault();
+    await addDoc(usersColRef, { name: name() });
+    setName("");
   };
-  getUsers();
-
-  //db
-  // const [application, setApplication] = createStore({
-  //   todos: [],
-  //   name: "",
-  //   user: "",
-  // });
-  // const [data, { mutate, refetch }] = createResource(fetchUser);
-
-  //fetch
+  const update = async (name: any, id: any) => {
+    const userDoc = doc(db, "users", id);
+    await updateDoc(userDoc, { name: name });
+  };
+  const deleteName = async (id: any) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  };
   return (
     <div>
-      <For each={state()}>{(e: any) => <>{e.name}</>}</For>
+      <For each={state()}>
+        {(e: any) => (
+          <div class="text-center">
+            {e.name}
+            <span>
+              <button onClick={() => update(name(), e.id)} style="color: red">
+                update
+              </button>
+              <button
+                onClick={() => deleteName(e.id)}
+                style="color: red; padding-left: 10px"
+              >
+                del
+              </button>
+            </span>
+          </div>
+        )}
+      </For>
       <section>
         <div class="text-center">Create Application</div>
         <div class="flex justify-between">
           <h2>let's go discuss your task</h2>
           <p>talk about the project</p>
         </div>
+        <form onSubmit={add}>
+          <input
+            type="name"
+            placeholder="name"
+            value={name()}
+            onInput={(e: any) => setName(e.currentTarget.value)}
+          />
+          {name()}
+        </form>
       </section>
     </div>
   );
