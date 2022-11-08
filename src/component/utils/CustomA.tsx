c050a46532da6826ac5dad009f65e29d1cd96e8c
kekelberi <alexei.gaponov@gmail.com>
2022-11-07 23:39:25 -0600

    save

--- src/component/utils/CustomA.tsx
+++ src/component/utils/CustomA.tsx
@@ -3,7 +3,7 @@ import { A } from "@solidjs/router";
 const CustomA = ({ name, path }: { name: string; path: string }) => {
   return (
     <A
-      class="group  z-0 relative inline-block overflow-hidden border sm:bg-yellow-300 border-yellow-600 px-2 py-3 focus:outline-none focus:ring"
+      class="group   relative inline-block overflow-hidden border sm:bg-yellow-300 border-yellow-600 px-2 py-3 focus:outline-none focus:ring"
       href={`${path}`}
     >
       <div class="absolute block inset-y-0 left-0 w-[2px] bg-yellow-600 transition-all group-hover:w-full  group-active:bg-yellow-600"></div>
