7cf9caa9bfbe9192463f0d8678af3eb5aa78174d
kekelberi <alexei.gaponov@gmail.com>
2022-11-07 23:53:59 -0600

    save

--- src/component/utils/CustomBut.tsx
+++ src/component/utils/CustomBut.tsx
@@ -1,6 +1,6 @@
 const CustomBut = ({ name }: { name: string }) => {
   return (
-    <div class="group  relative inline-block overflow-hidden border sm:bg-yellow-300 border-yellow-600 px-2 py-3 focus:outline-none focus:ring">
+    <div class="group z-0 relative inline-block overflow-hidden border sm:bg-yellow-300 border-yellow-600 px-2 py-3 focus:outline-none focus:ring">
       <div class="absolute block inset-y-0 left-0 w-[2px] bg-yellow-600 transition-all group-hover:w-full  group-active:bg-yellow-600"></div>
       <div class="relative block text-sm font-medium text-black transition-colors  group-hover:text-white">
         {name}
