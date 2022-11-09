b62d8c7a0b0fe7a7126616169f89ab1487ca7ebe
kekelberi <alexei.gaponov@gmail.com>
2022-11-09 16:02:28 -0600

    save

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -8,7 +8,6 @@ import Card from "../utils/Card";
 
 const Header: Component = () => {
   const [open, setOpen] = createSignal(false);
-
   const Show_ = Show as any; // cast as any
   const green = "text-green-400";
   const blue = "text-blue-400";
@@ -20,40 +19,20 @@ const Header: Component = () => {
 
   return (
     <div class={s.header_main}>
-      <div class="bg-black bg-opacity-25 text-white">
-        <A href="/">"{"YOUR SUCCESS"}"</A>
-        <h2>We help businesses turn ideas into effective products</h2>
-        <ul>
-          {}
-          {user?.user ? (
-            <button onClick={dest}>Sign Out</button>
-          ) : (
-            <>
-              <A href="/login">
-                <span class={green}>#</span>Login
-              </A>
-              <A href="/register">
-                <span class={blue}>#</span>Create Account
-              </A>
-            </>
-          )}
-
-          <span class={s.burger}>
-            <Show_
-              when={open()}
-              fallback={
-                <button onClick={() => setOpen((p) => (p = !p))}>open</button>
-              }
-            >
-              <button onClick={() => setOpen((p) => (p = !p))}>closed</button>
-              <Burger open={open()} setIsOpen={setOpen}>
-                <Card />
-                <Card />
-              </Burger>
-            </Show_>
-          </span>
-        </ul>
-      </div>
+      <span class={s.burger}>
+        <Show_
+          when={open()}
+          fallback={
+            <button onClick={() => setOpen((p) => (p = !p))}>open</button>
+          }
+        >
+          <button onClick={() => setOpen((p) => (p = !p))}>closed</button>
+          <Burger open={open()} setIsOpen={setOpen}>
+            <Card />
+            <Card />
+          </Burger>
+        </Show_>
+      </span>
     </div>
   );
 };
