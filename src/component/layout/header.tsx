fd1c6f8ca24c95c7633deab4ddd047a3c136658a
kekelberi <alexei.gaponov@gmail.com>
2022-11-09 21:08:45 -0600

    save

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -1,26 +1,35 @@
-import { A, useNavigate } from "@solidjs/router";
-import { parseCookies, destroyCookie } from "nookies";
-import { Component, createEffect, createSignal, Show } from "solid-js";
+import { destroyCookie } from "nookies";
+import { Component, createMemo, createSignal, Show } from "solid-js";
 import { useContextUser } from "../../context";
 import s from "../../scss/layout.module.scss";
 import Burger from "../utils/burger";
 import Card from "../utils/Card";
-import Ham2 from "./ham2";
 
 const Header: Component = () => {
   const [open, setOpen] = createSignal(false);
   const Show_ = Show as any; // cast as any
   const [user, { logOut }] = useContextUser() as any;
-
   const dest = () => {
     destroyCookie(null, "token"), destroyCookie(null, "email");
     logOut();
   };
+  const intervalBurger = () => {
+    setOpen((p) => (p = !p));
+  };
+
+  const Shows = () => (
+    <Show_ when={open()}>
+      <Burger open={open()} setIsOpen={setOpen}>
+        <Card />
+        <Card />
+      </Burger>
+    </Show_>
+  );
   return (
     <div class={s.header_main}>
       <span class={s.burger}>
         <div
-          onClick={() => setOpen((p) => (p = !p))}
+          onClick={intervalBurger}
           class={`${open() && "open "}`}
           id="nav-icon1"
         >
@@ -28,12 +37,7 @@ const Header: Component = () => {
           <span></span>
           <span></span>
         </div>
-        <Show_ when={open()} class="transition delay-700 duration-300">
-          <Burger open={open()} setIsOpen={setOpen}>
-            <Card />
-            <Card />
-          </Burger>
-        </Show_>
+        <Shows />
       </span>
     </div>
   );
