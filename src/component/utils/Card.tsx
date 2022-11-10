2530ee1bff457707164dbff4797b0f29d88618da
kekelberi <alexei.gaponov@gmail.com>
2022-11-09 22:03:11 -0600

    save

--- src/component/utils/Card.tsx
+++ src/component/utils/Card.tsx
@@ -3,7 +3,7 @@ const Card = ({ name = "test full asdasd", url = "" }) => {
     <div>
       <div class="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
         <div class="animate-pulse flex space-x-4">
-          <div class="rounded-full bg-gray-400 h-12 w-12">{name}</div>
+          <div class="rounded-full bg-gray-400 h-12 w-12"></div>
           <div class="flex-1 space-y-4 py-1">
             <div class="h-7 bg-gray-400 rounded w-3/4">{name}</div>
             <div class="space-y-2">
