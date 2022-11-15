c55a2438ec719a72a32d0ba35a857a0c54cc0a3a
kekelberi <alexei.gaponov@gmail.com>
2022-11-14 22:37:31 -0600

    save

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "@solidjs/router";
 import { Component } from "solid-js";
 import Layout from "./component/layout";
 import Pages from "./Pages";
-import GaponovAlexey from './person/GaponovAlexey.jsx'
 const App: Component = () => {
   return (
     <>
