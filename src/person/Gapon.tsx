a8a46a2a05d7063039a917f89768f445b48f10ce
kekelberi <alexei.gaponov@gmail.com>
2022-11-14 23:24:55 -0600

    save

--- src/person/Gapon.tsx
+++ src/person/Gapon.tsx
@@ -3,11 +3,18 @@ import s from "../scss/personal.module.scss";
 const Gapon = () => {
   return (
     <div class={s.personal}>
-      <div class={s.wrapper} >
-
-      <h1>Alexey Gaponov</h1>
-      <h2>Owner of Developer Experience at Sitesess.ca</h2>
-      <h3>Where we develop technologies that empower for business and celebrity</h3>
+      <div class={s.wrapper}>
+        <div class={s.top_block}>
+          <div>
+            <h1>Alexey Gaponov</h1>
+            <h2>Owner of Developer Experience at Sitesess.ca</h2>
+            <h3>
+              Where we develop technologies that empower for business and
+              celebrity
+            </h3>
+          </div>
+          <div class={s.top_img}></div>
+        </div>
       </div>
     </div>
   );
