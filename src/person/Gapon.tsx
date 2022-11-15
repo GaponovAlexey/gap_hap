d7e98ba7f1e78862dd1bfe7432937227a9430360
kekelberi <alexei.gaponov@gmail.com>
2022-11-14 22:34:00 -0600

    theme

--- src/person/Gapon.tsx
+++ src/person/Gapon.tsx
@@ -1,7 +1,10 @@
+import s from "../scss/personal.module.scss";
 
 const Gapon = () => {
-return (
-<div>it's me</div>
-)}
+  return <div class={s.personal}>
+    <h1>Alexey Gaponov</h1>
+    <h2>Owner of Developer Experience at Sitesess.ca</h2>
+  </div>;
+};
 
-export default Gapon
+export default Gapon;
