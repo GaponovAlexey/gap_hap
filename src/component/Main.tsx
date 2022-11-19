3400dec55b10e12fd5741a9c665538a14017728a
kekelberi <alexei.gaponov@gmail.com>
2022-11-19 14:05:14 -0600

    save

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,6 +1,5 @@
-import { createEffect, Match, Switch } from "solid-js";
+import { createEffect } from "solid-js";
 import s from "../scss/main.module.scss";
-import Application from "./page/Application";
 import AppliMap from "./page/AppliMap";
 import LandingUse from "./page/LandingUse";
 import Marketing from "./page/Marketing";
@@ -59,115 +58,115 @@ const PageWhite = () => {
   );
 };
 
-const MainPageTwo = () => {
-  return (
-    <div id="products" class={s.container_main_two}>
-      <div class={s.img} />
-      <span>
-        <h2>Your website is costing you customers</h2>
-        <h3>We can make you</h3>
-        <ul>
-          <a href="#Landing_block">landing</a>
-          <a href="#webServices">Web Services</a>
-          <a href="#Marketing_Block">Digital</a>
-        </ul>
-        <div class={s.services}>
-          <Switch fallback={<Landing_block />}>
-            <Match when={matches("landing")}>
-              <Landing_block />
-            </Match>
-            <Match when={matches("webServices")}>
-              <WebServices />
-            </Match>
-            <Match when={matches("Marketing_Block")}>
-              <Marketing_Block />
-            </Match>
-          </Switch>
-        </div>
-      </span>
-    </div>
-  );
-};
+// const MainPageTwo = () => {
+//   return (
+//     <div id="products" class={s.container_main_two}>
+//       <div class={s.img} />
+//       <span>
+//         <h2>Your website is costing you customers</h2>
+//         <h3>We can make you</h3>
+//         <ul>
+//           <a href="#Landing_block">landing</a>
+//           <a href="#webServices">Web Services</a>
+//           <a href="#Marketing_Block">Digital</a>
+//         </ul>
+//         <div class={s.services}>
+//           <Switch fallback={<Landing_block />}>
+//             <Match when={matches("landing")}>
+//               <Landing_block />
+//             </Match>
+//             <Match when={matches("webServices")}>
+//               <WebServices />
+//             </Match>
+//             <Match when={matches("Marketing_Block")}>
+//               <Marketing_Block />
+//             </Match>
+//           </Switch>
+//         </div>
+//       </span>
+//     </div>
+//   );
+// };
 
-const Landing_block = () => {
-  SlowScroll();
-  return (
-    <div class={s.two_text_services}>
-      <p>
-        Landing pages keep visitors focused on the specific thing you want them
-        to do, getting you better results with the same campaign budget.
-      </p>
-      <p>
-        Unlike web pages, which typically have many goals and encourage
-        exploration, landing pages are designed with a single focus or goal,
-        known as a call to action (or CTA, for short)
-      </p>
-      <p>
-        It’s this focus that makes landing pages the best option for increasing
-        the conversion rates of your marketing campaigns and lowering your cost
-        of acquiring a lead or sale.
-      </p>
-      <p class={s.but}>
-        {/* <CustomA name="Contact manager" path="landing" /> */}
-        <a href="#application" class="js-scroll">
-          Connect project manager
-        </a>
-      </p>
-    </div>
-  );
-};
+// const Landing_block = () => {
+//   SlowScroll();
+//   return (
+//     <div class={s.two_text_services}>
+//       <p>
+//         Landing pages keep visitors focused on the specific thing you want them
+//         to do, getting you better results with the same campaign budget.
+//       </p>
+//       <p>
+//         Unlike web pages, which typically have many goals and encourage
+//         exploration, landing pages are designed with a single focus or goal,
+//         known as a call to action (or CTA, for short)
+//       </p>
+//       <p>
+//         It’s this focus that makes landing pages the best option for increasing
+//         the conversion rates of your marketing campaigns and lowering your cost
+//         of acquiring a lead or sale.
+//       </p>
+//       <p class={s.but}>
+//         {/* <CustomA name="Contact manager" path="landing" /> */}
+//         <a href="#application" class="js-scroll">
+//           Connect project manager
+//         </a>
+//       </p>
+//     </div>
+//   );
+// };
 
-const WebServices = () => {
-  SlowScroll();
-  return (
-    <div class={s.two_text_services}>
-      <p>
-        A Web service is any software, application or cloud technology system
-        built for interoperable machine-to-machine or application-to-application
-        interaction over a network. They allow different applications to
-        seamlessly communicate and share data with each other.
-      </p>
-      <p>
-        Web services provide a common platform that allows multiple applications
-        built on various programming languages to have the ability to
-        communicate with each other.
-      </p>
-      <p>We will advise you and find the best solution for you</p>
-      <p class={s.but}>
-        <a href="#application" class="js-scroll">
-          Connect project manager
-        </a>
-      </p>
-    </div>
-  );
-};
+// const WebServices = () => {
+//   SlowScroll();
+//   return (
+//     <div class={s.two_text_services}>
+//       <p>
+//         A Web service is any software, application or cloud technology system
+//         built for interoperable machine-to-machine or application-to-application
+//         interaction over a network. They allow different applications to
+//         seamlessly communicate and share data with each other.
+//       </p>
+//       <p>
+//         Web services provide a common platform that allows multiple applications
+//         built on various programming languages to have the ability to
+//         communicate with each other.
+//       </p>
+//       <p>We will advise you and find the best solution for you</p>
+//       <p class={s.but}>
+//         <a href="#application" class="js-scroll">
+//           Connect project manager
+//         </a>
+//       </p>
+//     </div>
+//   );
+// };
 
-const Marketing_Block = () => {
-  SlowScroll();
-  return (
-    <div class={s.two_text_services}>
-      <p>
-        Digital marketing is the promotion of a company and its products or
-        services through online tools that generate leads, drive traffic, and
-        boost sales.
-      </p>
-      <p>
-        Now, businesses can target their audience with pinpoint accuracy and
-        provide useful information that resonates.
-      </p>
-      <p>
-        This is perfect because that’s exactly what today’s consumers want.
-        People don’t want to hear about products and services that don’t
-        interest them.
-      </p>
-      <p class={s.but}>
-        <a href="#application" class="js-scroll">
-          Connect project manager
-        </a>
-      </p>
-    </div>
-  );
-};
+// const Marketing_Block = () => {
+//   SlowScroll();
+//   return (
+//     <div class={s.two_text_services}>
+//       <p>
+//         Digital marketing is the promotion of a company and its products or
+//         services through online tools that generate leads, drive traffic, and
+//         boost sales.
+//       </p>
+//       <p>
+//         Now, businesses can target their audience with pinpoint accuracy and
+//         provide useful information that resonates.
+//       </p>
+//       <p>
+//         This is perfect because that’s exactly what today’s consumers want.
+//         People don’t want to hear about products and services that don’t
+//         interest them.
+//       </p>
+//       <p class={s.but}>
+//         <a href="#application" class="js-scroll">
+//           Connect project manager
+//         </a>
+//       </p>
+//     </div>
+//   );
+// };
 //4
 const MainPageFour = () => {
   return (
@@ -195,36 +194,36 @@ const MainPageFour = () => {
   );
 };
 //5
-const MainPageFive = () => {
-  return (
-    <div class={s.container_main_five}>
-      <div class={s.img}>
-        <div class={s.wrap}>
-          <h2>Our advantages</h2>
-          <ul class={s.wrap_advantages}>
-            <li class={s.one}>
-              We will make an adaptive design for any type of device
-            </li>
-            <li class={s.two}>
-              We will install analytics counters on the site and set up the
-              ability to track the results directly from your mobile phone
-            </li>
-            <li class={s.three}>
-              We will set up all possible feedback methods: mail, online
-              consultant, phone call from the site,{" "}
-              <span class="text-[#48C857]">WhatsApp</span>,
-              <span class="text-[#29A9EB]">Telegram</span> connected to the site
-            </li>
-            <li class={s.four}>
-              We will analyze your industry and competitors, offer the best
-              tools for promotion on the Internet
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const MainPageFive = () => {
+//   return (
+//     <div class={s.container_main_five}>
+//       <div class={s.img}>
+//         <div class={s.wrap}>
+//           <h2>Our advantages</h2>
+//           <ul class={s.wrap_advantages}>
+//             <li class={s.one}>
+//               We will make an adaptive design for any type of device
+//             </li>
+//             <li class={s.two}>
+//               We will install analytics counters on the site and set up the
+//               ability to track the results directly from your mobile phone
+//             </li>
+//             <li class={s.three}>
+//               We will set up all possible feedback methods: mail, online
+//               consultant, phone call from the site,{" "}
+//               <span class="text-[#48C857]">WhatsApp</span>,
+//               <span class="text-[#29A9EB]">Telegram</span> connected to the site
+//             </li>
+//             <li class={s.four}>
+//               We will analyze your industry and competitors, offer the best
+//               tools for promotion on the Internet
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 const SlowScroll = () =>
   createEffect(() => {
