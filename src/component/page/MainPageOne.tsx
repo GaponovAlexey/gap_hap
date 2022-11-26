75be4b3acb8303a418e3154a6801e43b4273ad17
kekelberi <alexei.gaponov@gmail.com>
2022-11-26 16:40:49 -0600

    save

--- src/component/page/MainPageOne.tsx
+++ src/component/page/MainPageOne.tsx
@@ -4,13 +4,17 @@ import CustomBut from "../utils/CustomBut";
 import s from "../../scss/main.module.scss";
 
 const MainPageOne: Component = () => {
-
-  
   return (
     <div class={s.container_main}>
       <div id="home">
         <h3>The development company</h3>
-        <Motion.h1  initial={{ y: -400 }} animate={{ y: 0 }} transition={{delay:  0.1}}>Sitesess.ca</Motion.h1>
+        <Motion.h1
+          initial={{ y: -400 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 0.1 }}
+        >
+          Sitesess.ca
+        </Motion.h1>
         <h2>We Know You Are Passionate About Your Small Business</h2>
         <h4>We help businesses turn ideas into effective products</h4>
         <div class="text-center pt-10 hover:scroll-auto">
